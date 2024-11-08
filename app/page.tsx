aaca42480dfe09f8edc31c92e6e51bfbf930a127
Ben Hood <benhood97@btinternet.com>
2024-11-08 14:48:36 -0800

    fix:styling on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Contact from "./components/Contact";
 
 export default function Home() {
   return (
-    <div className="mt-14">
-      <section id="home" className="relative z-10 min-h-screen">
+    <div className="mt-14 w-full">
+      <section id="home" className="relative z-10 min-h-screen h-full w-full">
         <Image
           src={"/pictures/Chairlift1.png"}
           height={700}
@@ -17,8 +17,8 @@ export default function Home() {
         <h1 className="relative z-10 text-black text-5xl font-bold text-center pt-40">
           Slope Side Support Society
         </h1>
-        <div className="absolute bottom-0 w-full h-1/3 bg-stone-50 flex items-center justify-center border-t-2 pb-5 pr-5 pl-5">
-          <h3 className="relative z-10 text-black font-semibold text-center text-lg lg:text-2xl mb-5">
+        <div className="absolute bottom-0 w-full h-1/3 bg-stone-50 flex items-center justify-center border-t-2 mb-5 pb-5 lg:pr-5 lg:pl-5">
+          <h3 className="relative z-10 text-black font-semibold text-center text-base lg:text-2xl mb-5">
             Slope Side Support Society is a non-profit organization dedicated to
             supporting men’s mental health through the power of skiing.
             <br /> We provide free group therapy sessions and individual support
