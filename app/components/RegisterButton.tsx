536670d219e21de41be96a6ca11e947db140763a
Ben Hood <benhood97@btinternet.com>
2025-05-26 11:25:34 -0700

    fit register button width

--- app/components/RegisterButton.tsx
+++ app/components/RegisterButton.tsx
@@ -7,7 +7,7 @@ export default function RegisterButton() {
     //   </button>
     // </a>
     <div className="flex items-center justify-center">
-      <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-400 w-44">
+      <div className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-400 w-fit">
         Coming Winter 2026
       </div>
     </div>
